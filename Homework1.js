8fa80ddb17931652137a749f2d08bc06b4114027
Anna Burtnyk <aburt@softserveinc.com>
2025-03-06 02:47:05 +0200

    Homework1

---
+++ Homework1.js
@@ -0,0 +1,9 @@
+//Task 1. Напишіть програму мовою JavaScript, яка перетворює вхідне числове значення в стрінгу, яка містить відповідну кількість годин і хвилин.
+
+//Приклад роботи:
+//inputValue = 82;
+//result: 1:22
+
+const inputValue = 82;
+const hours = inputValue/60
+const minutes = hours/60
