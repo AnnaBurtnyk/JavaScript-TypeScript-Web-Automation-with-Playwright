526447353212dbbc366eb9434df84474493ab807
Anna Burtnyk <aburt@softserveinc.com>
2025-03-18 12:42:25 +0200

    HW 4

--- Homework4.js
+++ Homework4.js
@@ -1,16 +1,5 @@
 // //Task 1. Для заданого об’єкта scores видалити властивості, значення яких є нечисловими. Вивести змінений об’єкт.
 
-// const scores = {
-// programming: 88,
-// design: 74,
-// jurisprudence: false,
-// relational_DB: 92,
-// computerGraphics: "submitted",
-// patterns: 65,
-// philosophy: "transferred",
-// network: 81
-// };
-
 const scores = {
     programming: 88,
     design: 74,
